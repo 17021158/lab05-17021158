02c3ad829386d410fb2cc5a5321817cc910ef30f
17021158 <43057126+17021158@users.noreply.github.com>
2018-10-25 21:15:16 +0700

    Hoàng Ngọc Huyền

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[]args){
+        Cam x= new Cam();
+        x.in();
+        Tao y=new Tao();
+        y.in();
+        Cam_sanh z=new Cam_sanh();
+        z.in();
+        Cam_cao_phong t=new Cam_cao_phong();
+        t.in();
+    }
+}
---
+++ src/Tao.java
@@ -0,0 +1,12 @@
+public class Tao extends Hoaqua {
+    public Tao(){
+        this.setGia_tien(20);
+        this.setNgay_nhap("1/1/2018");
+        this.setNguon_goc("VN");
+        this.setSo_luong(2);
+    }
+    public void in(){
+        System.out.println("Ten qua: tao");
+        super.in();
+    }
+}
